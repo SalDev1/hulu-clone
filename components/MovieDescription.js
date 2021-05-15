6fa64b754743be5b83baeca0476d7f33316f87b4
Salman Uddin <salmanuddin9820@gmail.com>
2021-05-15 12:12:59 +0530

    initial commit

---
+++ components/MovieDescription.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+function MovieDescription({ result }) {
+  console.log(result);
+  const BASE_URL = "https://image.tmdb.org/t/p/original";
+  return <div></div>;
+}
+
+export default MovieDescription;
